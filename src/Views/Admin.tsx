39fe5a13c1339e18a5bd54ef6a8555f66ddc8fe9
KazuBurrows <kazuiscool@gmail.com>
2025-05-13 20:18:00 +1200

    fixed commit

--- src/Views/Admin.tsx
+++ src/Views/Admin.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Driver } from "../types/models";
+// import { Driver } from "../types/models";
 import AdminNav from "../Components/Admin/AdminNav";
 import AdminBookings from "../Components/Admin/AdminBookings";
 
